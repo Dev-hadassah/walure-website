cf19b948a4c3ad80090ee08bbcc5029d76f9f4c5
Dev-hadassah <faridathadassah4@gmail.com>
2023-10-06 12:07:39 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: ' #982A90',
+        lilac: '#F8F3FF',
+        pink: '#FA48E8',
+        lightpurple: '#AE2EA5',
+        angularpeach: '#FCE3E4',
+        reactBlue: '#7DE1FC',
+        blazorpurple: '#702AF7',
+        vueGreen: '#41B883',
+        cream: '#Fef4db',
+        navyBlue: '#2E3A59',
+        skyBlue: '#F4F7FB',
+        orange: '#FF9201',
+        heroBlue: '#2489FF',
+        gradientBlue: '#1C72B1',
+        yellow: ' #FFDE88',
+        blue: '#CDE4F2',
+        purple2: '#7F56D9',
+        gray6: '#F2F2F2',
+        gray5: '#E0E0E0',
+        green: '#0F9403',
+        red: '#EC093F',
+        limeGreen: '#00A6A6',
+        yellow500: '#FFB905',
+        purple200: '#EAD4E9',
+        walureGreen: '#014B4B',
+        iposBlue: '#0779BF',
+        walureGray5: '#98A2B3',
+        text600: '#52525B',
+        text500: '#667085',
+        text400: ' #8B98B5',
+        text300: '#AABBDE',
+        text100: '#F2F6FF',
+        text200: '#E0EAFF',
+        text900: '#101828'
+      },
+      fontFamily: {
+      Montserrat: ['Montserrat', 'sans-serif'],
+      Inter: ['Inter', 'sans-serif'],
+      Jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    }
+  },
+  plugins: [],
+}
+
